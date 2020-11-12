e454fec11bd2d60ae7ae1f427ad6df366786be6c
Otakar Hirš <otakar.hirs@gmail.com>
2020-11-12 20:26:58 +0100

    feat(DAL): add repository method example

--- DataAccessLayer/Repositories/Interfaces/IUserRepository.cs
+++ DataAccessLayer/Repositories/Interfaces/IUserRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace clubIS.DataAccessLayer.Repositories.Interfaces
 {
     public interface IUserRepository : IRepository<User>
     {
+        Task<IEnumerable<User>> GetAllNameAndRegNumOnly();
     }
 }
--- DataAccessLayer/Repositories/UserRepository.cs
+++ DataAccessLayer/Repositories/UserRepository.cs
@@ -1,16 +1,32 @@
 ﻿using clubIS.DataAccessLayer.Repositories.Interfaces;
 using DataAccessLayer;
 using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace clubIS.DataAccessLayer.Repositories
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
         public UserRepository(DataContext context) : base(context)
+        {  
+        }
+        public async Task<IEnumerable<User>> GetAllNameAndRegNumOnly()
         {
+            return await _entities
+                .Select(u => 
+                    new User
+                    {
+                        Firstname = u.Firstname,
+                        Surname = u.Surname,
+                        RegistrationNumber = u.RegistrationNumber
+                    }
+                )
+                .ToListAsync();
         }
     }
 }
