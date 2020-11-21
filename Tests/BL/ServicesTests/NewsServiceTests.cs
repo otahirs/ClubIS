166013cdcab772cb4f18f24dfed47e333bad15c1
Otakar Hirš <otakar.hirs@gmail.com>
2020-11-20 20:35:30 -0800

    feat(BL): add NewsService test

---
+++ Tests/BL/ServicesTests/NewsServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using clubIS.BusinessLayer.DTOs;
+using clubIS.BusinessLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using AutoMapper;
+using clubIS.BusinessLayer.MapperConfig;
+using FluentAssertions.Execution;
+using FluentAssertions;
+
+namespace clubIS.BusinessLayer.Tests.ServicesTests
+{
+    public class NewsServiceTests
+    {
+        private readonly Func<NewsService> _createNewsService = () =>
+        {
+            return new NewsService(TestUoWFactory.Create());
+        };
+
+        private readonly IMapper _mapper = new Mapper(new MapperConfiguration(AutoMapperConfig.ConfigureMapping));
+
+        [Fact]
+        public async Task Basic()
+        {
+            var origNews = new NewsEditDTO()
+            {
+                UserId = 1,
+                Date = DateTime.Now,
+                Title = "Super novinka!",
+                Text = "V sobotu 30. února rozdáváme 5kg kyblíky nutely zdarma :O "
+            };
+            NewsListDTO news;
+            using (var uow = TestUoWFactory.Create())
+            {
+                var ps = new NewsService(uow);
+                await ps.Create(origNews);
+                await uow.Save();
+                news = (await ps.GetAll()).Last();
+            }
+
+            using (new AssertionScope())
+            {
+                news.Date.Should().Be(origNews.Date);
+                news.Title.Should().Be(origNews.Title);
+                news.Text.Should().Be(origNews.Text);
+                news.UserName.Should().NotBeNullOrEmpty();
+            }
+        }
+
+    }
+}
---
+++ Tests/BL/TestUoWFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using clubIS.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace clubIS.BusinessLayer.Tests
+{
+    public static class TestUoWFactory
+    {
+        public static UnitOfWork Create()
+        {
+            var builder = new DbContextOptionsBuilder<DataContext>();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var dbContextOptions = builder.Options;
+            var context = new DataContext(dbContextOptions);
+            // Delete existing db before creating a new one
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            return new UnitOfWork(context);
+        }
+    }
+}
