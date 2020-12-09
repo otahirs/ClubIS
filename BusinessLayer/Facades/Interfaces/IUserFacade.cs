2dc7f796177a185b7af48946754947ca7d404ef2
Otakar Hirš <otakar.hirs@gmail.com>
2020-12-09 11:21:48 -0800

    feat(BL): add User facade

---
+++ BusinessLayer/Facades/Interfaces/IUserFacade.cs
@@ -0,0 +1,19 @@
+﻿using clubIS.BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clubIS.BusinessLayer.Facades.Interfaces
+{
+    interface IUserFacade : IDisposable
+    {
+        Task Create(UserEditDTO e);
+        Task<IEnumerable<UserListDTO>> GetAll();
+        Task<UserDetailDTO> GetById(int id);
+        Task Update(UserEditDTO e);
+        Task Delete(int id);
+        Task UpdateLogin(UserCredentialsEditDTO user);
+        Task<IEnumerable<UserEntriesSupervisedListDTO>> GetAllEntriesSupervisors();
+    }
+}
---
+++ BusinessLayer/Facades/UserFacade.cs
@@ -0,0 +1,66 @@
+﻿using clubIS.BusinessLayer.DTOs;
+using clubIS.BusinessLayer.Facades.Interfaces;
+using clubIS.BusinessLayer.Services.Interfaces;
+using clubIS.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clubIS.BusinessLayer.Facades
+{
+    public class UserFacade : IUserFacade
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserService _userService;
+        public UserFacade(IUnitOfWork unitOfWork, IUserService userService)
+        {
+            _unitOfWork = unitOfWork;
+            _userService = userService;
+        }
+
+        public async Task Create(UserEditDTO user)
+        {
+            await _userService.Create(user);
+            await _unitOfWork.Save();
+        }
+
+        public async Task Update(UserEditDTO user)
+        {
+            await _userService.Update(user);
+            await _unitOfWork.Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            await _userService.Delete(id);
+            await _unitOfWork.Save();
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+
+        public async Task<IEnumerable<UserListDTO>> GetAll()
+        {
+            return await _userService.GetAll();
+        }
+
+        public async Task<UserDetailDTO> GetById(int id)
+        {
+            return await _userService.GetById(id);
+        }
+
+        public async Task UpdateLogin(UserCredentialsEditDTO user)
+        {
+            await _userService.UpdateLogin(user);
+            await _unitOfWork.Save();
+        }
+
+        public async Task<IEnumerable<UserEntriesSupervisedListDTO>> GetAllEntriesSupervisors()
+        {
+            return await _userService.GetAllEntriesSupervisors();
+        }
+    }
+}
