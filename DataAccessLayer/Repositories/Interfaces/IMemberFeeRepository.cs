57110900d816e1f2f39e20c31b17ba658dae889b
Otakar Hirš <otakar.hirs@gmail.com>
2021-01-06 17:38:18 -0800

    feat(DL): MemberFee repository

---
+++ DataAccessLayer/Repositories/Interfaces/IMemberFeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ClubIS.CoreLayer.Entities;
+
+namespace ClubIS.DataAccessLayer.Repositories.Interfaces
+{
+    public interface IMemberFeeRepository : IRepository<MemberFee>
+    {
+    }
+}
---
+++ DataAccessLayer/Repositories/MemberFeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ClubIS.CoreLayer.Entities;
+using ClubIS.DataAccessLayer.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClubIS.DataAccessLayer.Repositories
+{
+    public class MemberFeeRepository : Repository<MemberFee>, IMemberFeeRepository
+    {
+        public MemberFeeRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
