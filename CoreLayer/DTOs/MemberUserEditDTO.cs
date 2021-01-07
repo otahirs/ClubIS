e36612abde3ba4e1278ced0b65fbaa86cc20a7a5
Otakar Hirš <otakar.hirs@gmail.com>
2021-01-07 14:12:21 +0100

    fix: last commit <3 M$

--- CoreLayer/DTOs/MemberUserEditDTO.cs
+++ CoreLayer/DTOs/MemberUserEditDTO.cs
@@ -5,21 +5,12 @@ using ClubIS.CoreLayer.Enums;
 
 namespace ClubIS.CoreLayer.DTOs
 {
-    public class UserEditDTO
+    public class MemberUserEditDTO
     {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Surname { get; set; }
-        public string RegistrationNumber { get; set; }
         public ISet<SiCardDTO> SiCards { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public string Nationality { get; set; }
         public Address Address { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public Gender Gender { get; set; }
-        public Licence Licence { get; set; }
-        public MemberFeeDTO MemberFee { get; set; }
-        public AccountState AccountState { get; set; }
     }
 }
