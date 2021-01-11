a630b4e77af38a0ea7a902639f36f57b0a5db6d7
Matěj Novotný <matej.novotnny@gmail.com>
2021-01-11 22:29:19 +0100

    feat: button functionality updated

--- CoreLayer/Enums/EntryStatus.cs
+++ CoreLayer/Enums/EntryStatus.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace ClubIS.CoreLayer.Enums
 {
-    public enum EntryStatus
+    public enum EntryStatus : int
     {
-        Ok,
-        NotSent,
-        Changed,
-        Removed
+        Ok = 0,
+        NotSent = 1,
+        Changed = 2,
+        Removed = 3
     }
 }
